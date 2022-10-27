72a21edd5c3df2d77c4544410b40a34dd2ad2826
Muhammed Aisha <aishatayinke9@gmail.com>
2022-10-27 06:14:17 +0100

    commit

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+// sidebar
+const bar = document.querySelector(".bar");
+const cancel = document.querySelector(".cancel");
+const sideBar = document.querySelector(".side-solid");
+
+
+function barkick()  {
+    bar.style.display = 'none'
+    cancel.style.display = 'block'
+    sideBar.style.display = 'block'
+  }
+  
+  function closekick() {
+    cancel.style.display = 'none'
+    bar.style.display = 'block'
+    sideBar.style.display = 'none'
+  }
+
+  // sideBar ends 
+
+
 // logos 
 
 const all = [
